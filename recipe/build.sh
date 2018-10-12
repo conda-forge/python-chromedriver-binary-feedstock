7c80f3352808fee161cc0228ffd8f94cb7901762
Igor T. Ghisi <igor.ghisi@gmail.com>
2018-10-11 22:36:13 -0300

    Run pip install in the new standard

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,4 @@ python $RECIPE_DIR/download-chromedriver.py
 chmod 755 chromedriver/chromedriver
 # Add chromedriver to PATH so chromedriver_binary install can find it
 export PATH=$PATH:$PWD/chromedriver
-python -m pip install --no-deps --ignore-installed .
+python -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
