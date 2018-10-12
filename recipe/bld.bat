ec92d27c95b43c50e6ca52ec8750d7e15a07a0a8
Igor T. Ghisi <igor.ghisi@gmail.com>
2018-10-11 22:59:38 -0300

    Run pip install with the new standard (win)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,4 +3,4 @@ if errorlevel 1 exit 1
 
 REM Add chromedriver to PATH so chromedriver_binary install can find it
 set PATH=%PATH%;%CD%\chromedriver
-python -m pip install --no-deps --ignore-installed .
+python -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
