79fab494a9147bbfba7e136bf6dd1c74a6f0a931
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-01-13 20:16:56 -0500

    appveyor has an older chromedriver

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -5,22 +5,26 @@ import sys
 # it doesn't appear they do patch releases
 CHROMEDRIVER_VERSION = ".".join(os.environ["PKG_VERSION"].split(".")[:2])
 
-# allow side-effects to happen
-import chromedriver_binary  # noqa
+path = subprocess.check_output(["chromedriver-path"])
 
-assert "chromedriver" in os.environ["PATH"], os.environ["PATH"]
 
 if sys.platform.startswith("linux"):
     # on conda-forge builds, glibc is too old. just look at the thing.
-    bin = subprocess.check_output(["which", "chromedriver"]).decode("utf-8").strip()
+    bin = subprocess.check_output(
+        ["which", "chromedriver"], env=dict(PATH=path)
+    ).decode("utf-8").strip()
     assert os.access(bin, os.X_OK), "not executable"
 
     with open(bin, "rb") as fp:
         assert (CHROMEDRIVER_VERSION + ".").encode("utf-8") in fp.read(), \
-            "version string doesn't appear"
+            "version string doesn't appear in " + bin
 else:
     # test if the command executes, and capture the output
-    output = subprocess.check_output(["chromedriver", "--version"]).decode("utf-8")
+    output = subprocess.check_output(
+        ["chromedriver", "--version"],
+        env=dict(PATH=path)
+    ).decode("utf-8")
 
     # test that we got a version that matches the upstream
-    assert output.startswith("ChromeDriver " + CHROMEDRIVER_VERSION + "."), output
+    assert output.startswith("ChromeDriver " + CHROMEDRIVER_VERSION + "."), \
+        "version string doesn't appear in " + output
