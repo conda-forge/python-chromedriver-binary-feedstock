0fec136cb172b96a909313e0cebd714a7bceb8e1
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-01-13 20:23:15 -0500

    move side-effect test to end

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -5,19 +5,21 @@ import sys
 # it doesn't appear they do patch releases
 CHROMEDRIVER_VERSION = ".".join(os.environ["PKG_VERSION"].split(".")[:2])
 
-path = subprocess.check_output(["chromedriver-path"])
+path = subprocess.check_output(["chromedriver-path"]).decode("utf-8").strip()
 
 
 if sys.platform.startswith("linux"):
     # on conda-forge builds, glibc is too old. just look at the thing.
     bin = subprocess.check_output(
-        ["which", "chromedriver"], env=dict(PATH=path)
+        ["which", "chromedriver"], env=dict(PATH=os.pathsep.join([
+            path, os.environ["PATH"]
+        ]))
     ).decode("utf-8").strip()
     assert os.access(bin, os.X_OK), "not executable"
 
     with open(bin, "rb") as fp:
         assert (CHROMEDRIVER_VERSION + ".").encode("utf-8") in fp.read(), \
-            "version string doesn't appear in " + bin
+            "version string doesn't appear in content of: " + bin
 else:
     # test if the command executes, and capture the output
     output = subprocess.check_output(
@@ -27,4 +29,10 @@ else:
 
     # test that we got a version that matches the upstream
     assert output.startswith("ChromeDriver " + CHROMEDRIVER_VERSION + "."), \
-        "version string doesn't appear in " + output
+        "version string doesn't appear in: " + output
+
+# finally test weird $PATH side-effect behavior
+os.environ["PATH"] = ""
+import chromedriver_binary  # noqa
+assert path in os.environ["PATH"], \
+    "Path didn't get installed by side-effect: " + os.environ["PATH"]
