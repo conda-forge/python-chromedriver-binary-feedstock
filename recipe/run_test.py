b3e3b0ddf866f65ab519f7138435e81e6d24120c
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-02-23 08:21:44 -0600

    update licenses

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -12,22 +12,23 @@ if subprocess.Popen([PATH_BIN]).wait() != 0:
     print("Couldn't even call " + PATH_BIN)
     sys.exit(1)
 
-path = str(
-    subprocess.check_output([PATH_BIN]).decode("utf-8").strip()
-)
+path = str(subprocess.check_output([PATH_BIN]).decode("utf-8").strip())
 
 if sys.platform.startswith("linux"):
     # on conda-forge builds, glibc is too old. just look at the thing.
-    bin = subprocess.check_output(
-        ["which", BIN], env=dict(PATH=os.pathsep.join([
-            path, os.environ["PATH"]
-        ]))
-    ).decode("utf-8").strip()
+    bin = (
+        subprocess.check_output(
+            ["which", BIN], env=dict(PATH=os.pathsep.join([path, os.environ["PATH"]]))
+        )
+        .decode("utf-8")
+        .strip()
+    )
     assert os.access(bin, os.X_OK), "not executable"
 
     with open(bin, "rb") as fp:
-        assert (VERSION + ".").encode("utf-8") in fp.read(), \
+        assert (VERSION + ".").encode("utf-8") in fp.read(), (
             "version string doesn't appear in content of: " + bin
+        )
 else:
     bin = os.path.join(path, BIN)
 
@@ -38,11 +39,14 @@ else:
     output = subprocess.check_output([bin, "--version"]).decode("utf-8")
 
     # test that we got a version that matches the upstream
-    assert (("ChromeDriver " + VERSION + ".") in output), \
+    assert ("ChromeDriver " + VERSION + ".") in output, (
         "version string doesn't appear in: " + output
+    )
 
 # finally test weird $PATH side-effect behavior
 os.environ["PATH"] = ""
 import chromedriver_binary  # noqa
-assert path in os.environ["PATH"], \
+
+assert path in os.environ["PATH"], (
     "Path didn't get installed by side-effect: " + os.environ["PATH"]
+)
